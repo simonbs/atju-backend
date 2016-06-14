c5dc213f63a424f7812b8271ba49a61ba895e8f8
Simon Støvring <mail@simonbs.dk>
2016-06-15 00:55:06 +0200

    Handles 404

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ var Jobs = require('./lib/jobs');
 
 app.use('/dmi', dmi);
 
+// Page not found.
+app.use(function(req, res, next) {
+  res.status(404).send({
+    error: 'Resource not found.'
+  });
+});
+
 models.sequelize.sync().then(function() {
   var port = process.env.PORT || 8080;
   app.listen(port);
