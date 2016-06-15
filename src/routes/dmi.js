16ad1f0f9f6dc5a9b891eb8548d1124e7582d6f8
Simon Støvring <mail@simonbs.dk>
2016-06-15 21:55:39 +0200

    Removes debug log

--- src/routes/dmi.js
+++ src/routes/dmi.js
@@ -5,7 +5,6 @@ var PollenStore = require('../lib/pollenstore');
 app.get('/pollen', function(req, res) {
   var pollenStore = new PollenStore();
   pollenStore.load(function(err, data) {
-    console.log(err);
     if (err) { return res.status(500).send({ error: err.message }); }
     res.send(data);
   });
