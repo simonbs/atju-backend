7ce8d7418fd563dee4f78b735f36bd97c1c96d31
Simon Støvring <mail@simonbs.dk>
2017-05-25 19:08:38 +0200

    Sets content headers

--- src/routes/dmi.js
+++ src/routes/dmi.js
@@ -6,7 +6,7 @@ app.get('/pollen', function(req, res) {
   var pollenStore = new PollenStore();
   pollenStore.load(function(err, data) {
     if (err) { return res.status(500).send({ error: err.message }); }
-    res.send(data);
+    res.json(data);
   });
 });
 
@@ -14,7 +14,7 @@ app.get('/pollen/history', function(req, res) {
   var pollenStore = new PollenStore();
   pollenStore.loadHistory(req.query.date, req.query.pollen_type, req.query.city, function(err, data) {
     if (err) { return res.status(500).send({ error: err.message }); }
-    res.send(data);
+    res.json(data);
   });
 });
 
