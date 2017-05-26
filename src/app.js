630e030d94a3147a1adc5829de7efef04eca068b
Simon Støvring <mail@simonbs.dk>
2017-05-26 19:14:18 +0200

    Removes jobs

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express();
 var models = require('./models');
 var dmi = require('./routes/dmi');
-var Jobs = require('./lib/jobs');
 
 app.get('/', (function(req, res) {
     res.send('atju-backend is running.');
@@ -22,7 +21,5 @@ models.sequelize.sync().then(function() {
   var port = process.env.PORT || 8080;
   app.listen(port);
   console.log('Listening on port ' + port + '...');
-  var jobs = new Jobs();
-  jobs.start();
 });
 
